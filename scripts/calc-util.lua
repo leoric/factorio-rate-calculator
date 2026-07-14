cf6a9596f1503ce98287a637124b2702758e63df
Borgoth <borgoth@noreply.codeberg.org>
2026-07-14 23:23:18 +0200

    Fix lab rates (#230)
    My first PR so don't beat me up if I did something wrong :)
    Entity.speed_bonus is total speed bonus multiplier, shown in game in percentage when you hover over lab, so you just need to add 1 to make code work.
    I've tested this on my PC with different configurations of labs and modules and it matches manual calculations and factoriolab.github.io
    
    Co-authored-by: Borgoth <borgoth@gmail.com>
    Reviewed-on: https://codeberg.org/raiguard/RateCalculator/pulls/230

--- scripts/calc-util.lua
+++ scripts/calc-util.lua
@@ -460,7 +460,7 @@ function calc_util.process_lab(set, entity, invert)
   -- XXX: Due to a bug with entity_speed_bonus, we must subtract the force's lab speed bonus and convert it to a
   -- multiplicative relationship
   local lab_multiplier = research_multiplier
-    * ((entity.speed_bonus + 1 - speed_modifier) * (speed_modifier + 1))
+    * (entity.speed_bonus + 1)
     * researching_speed
     * science_pack_drain
 
